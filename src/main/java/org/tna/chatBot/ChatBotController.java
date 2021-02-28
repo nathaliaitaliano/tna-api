c742faddc2d04c90bdcfa5ff389a0d51136d98ec
Fernando <fernandoasfj+github@gmail.com>
2021-02-28 15:49:51 +0100

    Fix warnings by setting consume and produced type

--- src/main/java/org/tna/chatBot/ChatBotController.java
+++ src/main/java/org/tna/chatBot/ChatBotController.java
@@ -1,13 +1,18 @@
 package org.tna.chatBot;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/message")
 public class ChatBotController {
 
   @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
   public Response process(ChatMessage message) {
     if (message.isEmpty()) {
       return Response.status(Response.Status.BAD_REQUEST).build();
