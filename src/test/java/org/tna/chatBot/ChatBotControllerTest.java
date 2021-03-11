9cade6b5d270cfa1c7ac79167921b67c2b8219a6
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-11 13:25:41 +0100

    fix indentation

--- src/test/java/org/tna/chatBot/ChatBotControllerTest.java
+++ src/test/java/org/tna/chatBot/ChatBotControllerTest.java
@@ -12,46 +12,46 @@ public class ChatBotControllerTest {
   @Test
   public void shouldAcceptPostsOfNewMessages() {
     given()
-      .when()
-        .contentType(ContentType.JSON)
-        .body("{\"content\": \"hello\"}")
-        .post("/message")
-      .then().statusCode(200);
+            .when()
+            .contentType(ContentType.JSON)
+            .body("{\"content\": \"hello\"}")
+            .post("/message")
+            .then().statusCode(200);
   }
 
   @Test
   public void shouldRejectPostsWithEmptyMessages() {
     given()
-      .when()
-        .contentType(ContentType.JSON)
-        .body("{}")
-        .post("/message")
-      .then().statusCode(400);
+            .when()
+            .contentType(ContentType.JSON)
+            .body("{}")
+            .post("/message")
+            .then().statusCode(400);
   }
 
   @Test
   public void shouldAnswerMessageWithEcho() {
     given()
-      .when()
-        .contentType(ContentType.JSON)
-        .body("{\"content\": \"hello\"}")
-        .post("/message")
-      .then()
-        .statusCode(200).and()
-        .body("answer", is("You said: hello")).and()
-        .contentType(ContentType.JSON);
+            .when()
+            .contentType(ContentType.JSON)
+            .body("{\"content\": \"hello\"}")
+            .post("/message")
+            .then()
+            .statusCode(200).and()
+            .body("answer", is("You said: hello")).and()
+            .contentType(ContentType.JSON);
   }
 
   @Test
   public void shouldAnswerBlankMessagesWithSadFace() {
     given()
-      .when()
-        .contentType(ContentType.JSON)
-        .body("{\"content\": \"\"}")
-        .post("/message")
-      .then()
-        .statusCode(200).and()
-        .body("answer", is(":(")).and()
-        .contentType(ContentType.JSON);
+            .when()
+            .contentType(ContentType.JSON)
+            .body("{\"content\": \"\"}")
+            .post("/message")
+            .then()
+            .statusCode(200).and()
+            .body("answer", is(":(")).and()
+            .contentType(ContentType.JSON);
   }
 }
