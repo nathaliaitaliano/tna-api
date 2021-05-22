2a727b92e62ef730eae1fa615f3f64a5bedc0a5a
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-05-22 15:27:26 +0200

    test for dice roll result response

--- src/test/java/org/tna/chatBot/ChatBotControllerTest.java
+++ src/test/java/org/tna/chatBot/ChatBotControllerTest.java
@@ -2,8 +2,11 @@ package org.tna.chatBot;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 @QuarkusTest
@@ -53,5 +56,31 @@ public class ChatBotControllerTest {
             .statusCode(200).and()
             .body("answer", is(":(")).and()
             .contentType(ContentType.JSON);
+  }
+
+  @Test
+  public void shouldAnswerAD6Roll() {
+    Response response = given()
+            .when()
+            .contentType(ContentType.JSON)
+            .body("{\"content\": \"d6\"}")
+            .post("/message");
+    ChatResponse chatResponse = response.body().as(ChatResponse.class);
+    Integer answer = Integer.valueOf(chatResponse.answer);
+
+    assertThat(answer, is(both(greaterThanOrEqualTo(1)).and(lessThanOrEqualTo(6))));
+  }
+
+  @Test
+  public void shouldAnswerAD20Roll() {
+    Response response = given()
+            .when()
+            .contentType(ContentType.JSON)
+            .body("{\"content\": \"d20\"}")
+            .post("/message");
+    ChatResponse chatResponse = response.body().as(ChatResponse.class);
+    Integer answer = Integer.valueOf(chatResponse.answer);
+
+    assertThat(answer, is(both(greaterThanOrEqualTo(1)).and(lessThanOrEqualTo(20))));
   }
 }
