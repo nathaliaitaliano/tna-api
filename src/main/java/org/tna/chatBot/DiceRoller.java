82f4d88b527128e241953765f1ccab0d71795f78
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-05-22 20:01:58 +0200

    changing method roll to get the sides from a dice parameter

--- src/main/java/org/tna/chatBot/DiceRoller.java
+++ src/main/java/org/tna/chatBot/DiceRoller.java
@@ -1,7 +1,9 @@
 package org.tna.chatBot;
 
+import static java.lang.Integer.parseInt;
+
 public class DiceRoller {
-  public static int roll(int sides ) {
-    return (int)(Math.random() * sides + 1);
-  }
+  public static int roll(String dice) {
+    int sides = parseInt(dice.substring(1));
+    return (int) (Math.random() * sides + 1); }
 }
