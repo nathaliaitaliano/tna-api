008b079c4a41decf24ab726dfc5e69c97b9d2c16
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-05-31 22:24:41 +0200

    Fix indentation

--- src/main/java/org/tna/chatBot/DiceRoller.java
+++ src/main/java/org/tna/chatBot/DiceRoller.java
@@ -5,5 +5,6 @@ import static java.lang.Integer.parseInt;
 public class DiceRoller {
   public static int roll(String dice) {
     int sides = parseInt(dice.substring(1));
-    return (int) (Math.random() * sides + 1); }
+    return (int) (Math.random() * sides + 1);
+  }
 }
