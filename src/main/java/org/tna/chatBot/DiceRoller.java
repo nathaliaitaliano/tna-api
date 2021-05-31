22409b2f6a5ca01839cf64c994704d9934e1e7be
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-05-31 22:39:48 +0200

    Random number generator should be passed as a param instead of initialized on method

--- src/main/java/org/tna/chatBot/DiceRoller.java
+++ src/main/java/org/tna/chatBot/DiceRoller.java
@@ -3,8 +3,8 @@ package org.tna.chatBot;
 import static java.lang.Integer.parseInt;
 
 public class DiceRoller {
-  public static int roll(String dice) {
+  public static int roll(String dice, RandomNumberGenerator randomNumberGenerator) {
     int sides = parseInt(dice.substring(1));
-    return new RandomNumberGenerator().generate(sides);
+    return randomNumberGenerator.generate(sides);
   }
 }
--- src/test/java/org/tna/chatBot/DiceRollerTest.java
+++ src/test/java/org/tna/chatBot/DiceRollerTest.java
@@ -12,13 +12,13 @@ public class DiceRollerTest {
 
   @Test
   public void shouldAnswerAD6Roll() {
-    int rollResult = DiceRoller.roll("d6");
+    int rollResult = DiceRoller.roll("d6", new RandomNumberGenerator());
     assertThat(rollResult, is(both(greaterThanOrEqualTo(1)).and(lessThanOrEqualTo(6))));
   }
 
   @Test
   public void shouldAnswer20Roll() {
-    int rollResult = DiceRoller.roll("d20");
+    int rollResult = DiceRoller.roll("d20", new RandomNumberGenerator());
     assertThat(rollResult, is(both(greaterThanOrEqualTo(1)).and(lessThanOrEqualTo(20))));
   }
 }
